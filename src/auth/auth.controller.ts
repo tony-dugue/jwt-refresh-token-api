b8caea9eb255bc296be68666cf5639f5d9a131e5
tony dugué <duguetony@gmail.com>
2022-06-10 23:39:24 +0200

    add signup route (+ logique & generate access & refresh tokens)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,29 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/local/signup')
-  signupLocal() {
-    this.authService.signupLocal();
+  signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
+    return this.authService.signupLocal(dto);
   }
 
   @Post('/local/signin')
   signinLocal() {
-    this.authService.signinLocal();
+    return this.authService.signinLocal();
   }
 
   @Post('/logout')
   logout() {
-    this.authService.logout();
+    return this.authService.logout();
   }
 
   @Post('/refresh')
   refreshTokens() {
-    this.authService.refreshTokens();
+    return this.authService.refreshTokens();
   }
 }
