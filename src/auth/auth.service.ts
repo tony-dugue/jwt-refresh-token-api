ec444b7c2df3f08d887b3220bc5d7a74b2890f5d
tony dugué <duguetony@gmail.com>
2022-06-11 11:06:21 +0200

    add signin route (+ logique & generate new refresh token hash)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
@@ -8,6 +8,55 @@ import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hash = await this.hashData(dto.password);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async signinLocal(dto: AuthDto): Promise<Tokens> {
+    // récupération du user dans la base de données
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (!user) throw new ForbiddenException('Accès refusé!');
+
+    // comparaison des mots de passe
+    const passwordMatches = await bcrypt.compare(dto.password, user.hash);
+    if (!passwordMatches) throw new ForbiddenException('Accès refusé!');
+
+    // génération de 2 nouveaux tokens
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  logout() {}
+
+  refreshTokens() {}
+
+  // Database modify
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { hashedRt: hash },
+    });
+  }
+
+  // Helpers
 
   hashData(data: string) {
     return bcrypt.hash(data, 10);
@@ -30,33 +79,4 @@ export class AuthService {
       refresh_token: rt,
     };
   }
-
-  async signupLocal(dto: AuthDto): Promise<Tokens> {
-    const hash = await this.hashData(dto.password);
-
-    const newUser = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-
-    const tokens = await this.getTokens(newUser.id, newUser.email);
-    await this.updateRtHash(newUser.id, tokens.refresh_token);
-    return tokens;
-  }
-
-  async updateRtHash(userId: number, rt: string) {
-    const hash = await this.hashData(rt);
-    await this.prisma.user.update({
-      where: { id: userId },
-      data: { hashedRt: hash },
-    });
-  }
-
-  signinLocal() {}
-
-  logout() {}
-
-  refreshTokens() {}
 }
